a67d77963b7b6ff951a10f39ebf8885d525b9bce
Edwardyooooooo <87703911+Edwardyooooooo@users.noreply.github.com>
2023-05-20 19:05:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -164,9 +164,9 @@ if __name__ == "__main__":
         exit(-1)
     stat, msg = s.book_favorite_seat(time_data[key]['开始时间'], time_data[key]['持续小时数'])
     if stat != "ok":
-        for i in range(30):
+        for i in range(35):
             print("尝试重新预约")
-            time.sleep(10)
+            time.sleep(11)
             stat, msg = s.book_favorite_seat(time_data[key]['开始时间'], time_data[key]['持续小时数'])
             print(stat, msg)
             if stat == "ok":
